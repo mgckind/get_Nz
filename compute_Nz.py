8456513e2813701f9c8575359e8d2eab5e874eb0
mgckind <mgckind@gmail.com>
2014-07-30 16:34:02 -0500

    Update all

--- compute_Nz.py
+++ compute_Nz.py
@@ -1,4 +1,5 @@
-# Code to compute N(z) from the sparse representation
+#!/usr/bin/python
+#Code to compute N(z) from the sparse representation
 # presented in http://adsabs.harvard.edu/abs/2014MNRAS.441.3550C
 # using TPZ (http://lcdm.astro.illinois.edu/code/mlz.html)
 # Comments/questions: Matias Carrasco Kind mcarras2@illinois.edu
@@ -10,34 +11,61 @@ import pyfits as pf
 import pdf_storage as ps
 import mask
 import sys,os
+import argparse
 
+class MyParser(argparse.ArgumentParser):
+    def error(self, message):
+        print '\n*****************'
+        sys.stderr.write('error: %s\n' % message)
+        print '***************\n'
+        self.print_help()
+        sys.exit(2)
 
-# This is the mask used to get N(z) and galaxies in general
-# for sva1_gold_1.0 all the objects
-# What's there is what's included
-# For a range of values used a [min_val, max_val]
-# For equalities used [value]
-# You should use only one of the S/G keys AND COMMENT THE OTHER ONE
-# For TPZ_SG_CLASS the range goes from 0 (galaxies) to 1 (stars)
+parser = MyParser(description='Compute N(z) for selected galaxies from SVA1 Gold 1.0 catalogs')
+parser.add_argument("cutsfile", help="Textfile file with cuts, check default.cuts for more info")
+parser.add_argument("-m","--mask", help="Numpy file with positional index mask, usually created using cutsfile, overrides cutsfile")
+parser.add_argument("-i", "--ids", help="Text file (ASCII) with COADD_OBJECTS_ID to be matched (slow), it returns the mask to be used with -m option")
+parser.add_argument("--path", help="Path to the catalogs (default is " + os.getcwd()+")", default=os.getcwd())
+parser.add_argument("--return_pdfs", action="store_true", help="Return a numpy file with the original PDFs for the selected objects, the last\
+                    row  is the redshift positions")
+parser.add_argument("--plot", action="store_true", help="Plot the N(z)")
+args = parser.parse_args()
 
+if args.mask != None and args.ids != None:
+    print '\n********************'
+    print 'Error: either --mask or --ids is acceptable, not both'
+    print '*********************\n'
+    parser.print_help()
+    sys.exit(2)
 
-#The default values are for the LSS benchmark cuts
+if args.path[-1]!='/': args.path = args.path + '/'
+
+F=open(args.cutsfile,'r')
+FM=F.readlines()
+F.close()
+
 Mk={}
-Mk['MAG_AUTO_I']=[18,22.5]
-Mk['RA']=[60,95]
-Mk['DEC']=[-62,-40]
-Mk['MODEST_CLASS']=[1] # USE MODEST_CLASS or TPZ_SG_CLASS between 0 (galaxies) and 1 (stars)
-#Mk['TPZ_SG_CLASS']=[0.0,0.8] #TPZ PROB S/G Classification
-Mk['TPZ_ZCONF']=[0.6,1.1]  #ZCONF, goodness of the PDF, quality cut
-Mk['TPZ_ZPHOT']=[0.2,0.4] #REDSHIFT BIN FROM TPZ
-Mk['MAG_DETMODEL_G-MAG_DETMODEL_R']=[0,3]
-Mk['MAG_DETMODEL_R-MAG_DETMODEL_I']=[0,2]
-Mk['MAG_DETMODEL_I-MAG_DETMODEL_Z']=[0,3]
+for Line in FM:
+    if Line[0]=='#' or Line[0]=='\n': continue
+    line=Line.strip().split()
+    name=line[0]
+    vals=line[2][1:-1]
+    if ',' in vals:
+        Mk[name]=[float(vals.split(',')[0]),float(vals.split(',')[1])]
+    else:
+        Mk[name]=[float(vals)]
 
 
-print '***  Creating mask...\n'
-for i in xrange(len(Mk)):print Mk.keys()[i],'=',Mk.values()[i]
-print
+if args.mask==None:
+    if args.ids==None:
+        print '\n***  Creating mask...\n'
+        for i in xrange(len(Mk)):print Mk.keys()[i],'=',Mk.values()[i]
+        print
+    else:
+        print '\n***  Creatin mask from ids from file:  '+args.ids+' ...\n'
+else:
+    print '\n***  Reading mask from : '+args.mask+' ...\n'
+    
     
 
 range_z='%.1f_%.1f' % (Mk['TPZ_ZPHOT'][0],Mk['TPZ_ZPHOT'][1])
@@ -46,19 +74,24 @@ range_z='%.1f_%.1f' % (Mk['TPZ_ZPHOT'][0],Mk['TPZ_ZPHOT'][1])
 # This includes, RA,DEC, MAG_AUTO_I, MAG_DETMODEL_[G,R,I,Z], MODEST_CLASS, COADD_OBJECTS_ID
 # and TPZ_ZPHOT (the mean of the redshift pdf) and TPZ_ZCONF (similar to ODDS in BPZ is a 0 to 1 value)
 # quantifying the shape of the pdf around the mean, usually one picks value with ZCONF > 0.5
-fitsfile='sva1_gold_1.0_short_sg.fits'
+fitsfile=args.path+'sva1_gold_1.0_short_sg.fits'
 
 # Return the mask given the cuts defined in Mk, output_keys are extra returns
 # (optional) masked, in that case those values will be: e.g.: Bmask['RA'] or
 # Bmask['TPZ_ZPHOT']
 
-
-Bmask=mask.get_mask(Mk, fitsfile, output_keys=['TPZ_ZPHOT'])
-masked_data=Bmask['MASK']
-save('masked_data_'+str(Mk['TPZ_ZPHOT'][0])+'_'+str(Mk['TPZ_ZPHOT'][1]),masked_data)
-# You can load the mask instead, comment the previous lines and uncomment the following
-# 
-#masked_data=load('masked_data_0.2_0.4.npy')
+if args.mask==None:
+    if args.ids==None:
+        Bmask=mask.get_mask(Mk, fitsfile, output_keys=['TPZ_ZPHOT'])
+        masked_data=Bmask['MASK']
+        save('masked_data_'+range_z,masked_data)
+    else:
+        Bmask=mask.get_mask(Mk, fitsfile, output_keys=['TPZ_ZPHOT'],input_ids=args.ids)
+        masked_data=Bmask['MASK']
+        save('masked_data_ids',masked_data)
+else:
+    masked_data=load(args.mask)
+    save('masked_data',masked_data)
 
 Ngal=len(masked_data)
 print '*** Number of galaxies : ', Ngal ; print
@@ -67,15 +100,27 @@ print '*** Number of galaxies : ', Ngal ; print
 # and get the indexes indicated by Bmask
 # And also reads the header
 print '*** Reading Sparse files...\n'
-P2=pf.open('sva1_gold_1.0.Psparse_all.fits')
+P2=pf.open(args.path+'sva1_gold_1.0.Psparse_all.fits')
 zz=P2[1].data.field('redshift')
 SP = P2[2].data.field('Sparse_indices')[masked_data]
 P2.close()
-head = ps.read_header('sva1_gold_1.0.Psparse_all.fits')
+head = ps.read_header(args.path+'sva1_gold_1.0.Psparse_all.fits')
 z = head['z']
 dz=zz[1]-zz[0]
 
 # This functions recovers the individual pdfs itself
+if args.return_pdfs:
+    print '*** Extracting original PDFs (slow)...\n'
+    P=[]
+    for kk in xrange(len(masked_data)):
+        rep_pdf = ps.reconstruct_pdf_int(SP[kk], head)
+        P.append(rep_pdf)
+    P.append(z)
+    if args.mask==None:
+        pdfout='PDFs_'+range_z+'_tpz'
+    else:
+        pdfout='PDFs_masked_tpz'
+    save(pdfout,array(P))
 #kk=111
 #rep_pdf = ps.reconstruct_pdf_int(SP[kk], head)
 #plt.plot(z,rep_pdf)
@@ -93,22 +138,37 @@ for i in xrange(Ngal): Coef[RR[i,1,:]]+=dVals*RR[i,0,:]
 N_z=dot(A,Coef)
 N_z=N_z/sum(N_z)/dz
 
-# Ploting dN/dz
-#
-plt.figure()
-plt.fill_between(z,N_z,alpha=0.5,facecolor='red')
-plt.xlabel(r'$z$',fontsize=20)
-plt.ylabel(r'$dn(z)/dz$',fontsize=20)
-plt.axvline(Mk['TPZ_ZPHOT'][0],linestyle='--',color='black')
-plt.axvline(Mk['TPZ_ZPHOT'][1],linestyle='--',color='black')
-plt.savefig('N_z_'+range_z+'_tpz.png',bbox_inches='tight')
 # Saving dN/dz to a file
 # the first column is the center of the dz bin and the second colum tne value
 # sum(N_z*dz) = 1
-with open('N_z_'+range_z+'_tpz.txt','wb') as fout:
+if args.mask==None:
+    fileout='N_z_'+range_z+'_tpz.txt'
+    maskout='masked_data_'+range_z+'.npy'
+else:
+    fileout='N_z_masked_tpz.txt'
+    maskout='masked_data.npy'
+    
+print '*** Output Files: \n'
+print '    N_z txt : '+fileout
+print '    Numpy mask : '+ maskout
+if args.return_pdfs:
+    print '    PDFs file : '+ pdfout
+    
+
+with open(fileout,'wb') as fout:
     fout.write(b'# zmid   n(z)\n')
     ngal='# No galaxies : %d \n' %Ngal
     fout.write(ngal)
     savetxt(fout,zip(z,N_z),fmt='%.6f')
-       
-plt.show()
+
+
+# Ploting dN/dz
+#
+if args.plot:
+    plt.figure()
+    plt.fill_between(z,N_z,alpha=0.5,facecolor='red')
+    plt.xlabel(r'$z$',fontsize=20)
+    plt.ylabel(r'$dn(z)/dz$',fontsize=20)
+    plt.axvline(Mk['TPZ_ZPHOT'][0],linestyle='--',color='black')
+    plt.axvline(Mk['TPZ_ZPHOT'][1],linestyle='--',color='black')
+    plt.show()
