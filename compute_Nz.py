4244603c10715c201e73281bca2c8b3df91f236e
Matias Carrasco Kind <mgckind@users.noreply.github.com>
2014-07-30 18:35:16 -0500

    Updated PEP8 format

--- compute_Nz.py
+++ compute_Nz.py
@@ -10,22 +10,29 @@ import matplotlib.pyplot as plt
 import pyfits as pf
 import pdf_storage as ps
 import mask
-import sys,os
+import sys, os
 import argparse
+
 
 class MyParser(argparse.ArgumentParser):
     def error(self, message):
         print '\n*****************'
         sys.stderr.write('error: %s\n' % message)
-        print '***************\n'
+        print '*****************\n'
         self.print_help()
         sys.exit(2)
 
-parser = MyParser(description='Compute N(z) for selected galaxies from SVA1 Gold 1.0 catalogs')
+
+parser = MyParser(description='Compute N(z) for selected galaxies from \
+SVA1 Gold 1.0 catalogs by using cuts on attributes, a pre-defined mask or a list \
+of coadd objects ids *Comments/questions: Matias Carrasco Kind mcarras2@illinois.edu*')
 parser.add_argument("cutsfile", help="Textfile file with cuts, check default.cuts for more info")
-parser.add_argument("-m","--mask", help="Numpy file with positional index mask, usually created using cutsfile, overrides cutsfile")
-parser.add_argument("-i", "--ids", help="Text file (ASCII) with COADD_OBJECTS_ID to be matched (slow), it returns the mask to be used with -m option")
-parser.add_argument("--path", help="Path to the catalogs (default is " + os.getcwd()+")", default=os.getcwd())
+parser.add_argument("-m", "--mask",
+                    help="Numpy file with positional index mask, usually created using cutsfile, overrides cutsfile")
+parser.add_argument("-i", "--ids",
+                    help="Text file (ASCII) with COADD_OBJECTS_ID to be matched (slow), it returns the mask to be used with -m option")
+parser.add_argument("--path", help="Path to the catalogs (default is " + os.getcwd() + ")", default=os.getcwd())
+parser.add_argument("--root", help="Root of output filenames")
 parser.add_argument("--return_pdfs", action="store_true", help="Return a numpy file with the original PDFs for the selected objects, the last\
                     row  is the redshift positions")
 parser.add_argument("--plot", action="store_true", help="Plot the N(z)")
@@ -34,141 +41,145 @@ args = parser.parse_args()
 if args.mask != None and args.ids != None:
     print '\n********************'
     print 'Error: either --mask or --ids is acceptable, not both'
-    print '*********************\n'
+    print '********************\n'
     parser.print_help()
     sys.exit(2)
 
-if args.path[-1]!='/': args.path = args.path + '/'
+if args.path[-1] != '/': args.path = args.path + '/'
 
-F=open(args.cutsfile,'r')
-FM=F.readlines()
+F = open(args.cutsfile, 'r')
+FM = F.readlines()
 F.close()
 
-Mk={}
+Mk = {}
 for Line in FM:
-    if Line[0]=='#' or Line[0]=='\n': continue
-    line=Line.strip().split()
-    name=line[0]
-    vals=line[2][1:-1]
+    if Line[0] == '#' or Line[0] == '\n': continue
+    line = Line.strip().split()
+    name = line[0]
+    vals = line[2][1:-1]
     if ',' in vals:
-        Mk[name]=[float(vals.split(',')[0]),float(vals.split(',')[1])]
+        Mk[name] = [float(vals.split(',')[0]), float(vals.split(',')[1])]
     else:
-        Mk[name]=[float(vals)]
+        Mk[name] = [float(vals)]
 
-
-if args.mask==None:
-    if args.ids==None:
+if args.mask == None:
+    if args.ids == None:
         print '\n***  Creating mask...\n'
-        for i in xrange(len(Mk)):print Mk.keys()[i],'=',Mk.values()[i]
+        for i in xrange(len(Mk)): print Mk.keys()[i], '=', Mk.values()[i]
         print
     else:
-        print '\n***  Creatin mask from ids from file:  '+args.ids+' ...\n'
+        print '\n***  Creatin mask from ids from file (slow):  ' + args.ids + ' ...\n'
 else:
-    print '\n***  Reading mask from : '+args.mask+' ...\n'
-    
-    
+    print '\n***  Reading mask from : ' + args.mask + ' ...\n'
 
-range_z='%.1f_%.1f' % (Mk['TPZ_ZPHOT'][0],Mk['TPZ_ZPHOT'][1])
+range_z = '%.1f_%.1f' % (Mk['TPZ_ZPHOT'][0], Mk['TPZ_ZPHOT'][1])
 
 # Name of the catalog with few columns,
 # This includes, RA,DEC, MAG_AUTO_I, MAG_DETMODEL_[G,R,I,Z], MODEST_CLASS, COADD_OBJECTS_ID
 # and TPZ_ZPHOT (the mean of the redshift pdf) and TPZ_ZCONF (similar to ODDS in BPZ is a 0 to 1 value)
 # quantifying the shape of the pdf around the mean, usually one picks value with ZCONF > 0.5
-fitsfile=args.path+'sva1_gold_1.0_short_sg.fits'
+fitsfile = args.path + 'sva1_gold_1.0_short_sg.fits'
 
 # Return the mask given the cuts defined in Mk, output_keys are extra returns
 # (optional) masked, in that case those values will be: e.g.: Bmask['RA'] or
 # Bmask['TPZ_ZPHOT']
 
-if args.mask==None:
-    if args.ids==None:
-        Bmask=mask.get_mask(Mk, fitsfile, output_keys=['TPZ_ZPHOT'])
-        masked_data=Bmask['MASK']
-        save('masked_data_'+range_z,masked_data)
+if args.mask == None:
+    if args.ids == None:
+        Bmask = mask.get_mask(Mk, fitsfile, output_keys=['TPZ_ZPHOT'])
+        masked_data = Bmask['MASK']
+        save('masked_data_' + range_z, masked_data)
     else:
-        Bmask=mask.get_mask(Mk, fitsfile, output_keys=['TPZ_ZPHOT'],input_ids=args.ids)
-        masked_data=Bmask['MASK']
-        save('masked_data_ids',masked_data)
+        Bmask = mask.get_mask(Mk, fitsfile, output_keys=['TPZ_ZPHOT'], input_ids=args.ids)
+        masked_data = Bmask['MASK']
+        save('masked_data_ids', masked_data)
 else:
-    masked_data=load(args.mask)
-    save('masked_data',masked_data)
+    masked_data = load(args.mask)
+    save('masked_data', masked_data)
 
-Ngal=len(masked_data)
-print '*** Number of galaxies : ', Ngal ; print
+Ngal = len(masked_data)
+print '*** Number of galaxies : ', Ngal;
+print
 
 # Read the sparse representation file
 # and get the indexes indicated by Bmask
 # And also reads the header
 print '*** Reading Sparse files...\n'
-P2=pf.open(args.path+'sva1_gold_1.0.Psparse_all.fits')
-zz=P2[1].data.field('redshift')
+P2 = pf.open(args.path + 'sva1_gold_1.0.Psparse_all.fits')
+zz = P2[1].data.field('redshift')
 SP = P2[2].data.field('Sparse_indices')[masked_data]
 P2.close()
-head = ps.read_header(args.path+'sva1_gold_1.0.Psparse_all.fits')
+head = ps.read_header(args.path + 'sva1_gold_1.0.Psparse_all.fits')
 z = head['z']
-dz=zz[1]-zz[0]
+dz = zz[1] - zz[0]
 
 # This functions recovers the individual pdfs itself
 if args.return_pdfs:
     print '*** Extracting original PDFs (slow)...\n'
-    P=[]
+    P = []
     for kk in xrange(len(masked_data)):
         rep_pdf = ps.reconstruct_pdf_int(SP[kk], head)
         P.append(rep_pdf)
     P.append(z)
-    if args.mask==None:
-        pdfout='PDFs_'+range_z+'_tpz'
+    if args.mask == None:
+        if args.ids == None:
+            pdfout = 'PDFs_' + range_z + '_tpz'
+        else:
+            pdfout = 'PDFs_masked_ids_tpz'
     else:
-        pdfout='PDFs_masked_tpz'
-    save(pdfout,array(P))
+        pdfout = 'PDFs_masked_tpz'
+    save(pdfout, array(P))
 #kk=111
 #rep_pdf = ps.reconstruct_pdf_int(SP[kk], head)
 #plt.plot(z,rep_pdf)
 
 print '*** Creating Dictionary\n'
-A=ps.create_voigt_dict(head['z'],head['mu'],head['Nmu'],head['sig'],head['Nsig'],head['Nv'])
+A = ps.create_voigt_dict(head['z'], head['mu'], head['Nmu'], head['sig'], head['Nsig'], head['Nv'])
 VALS = linspace(0, 1, head['Ncoef'])
 dVals = VALS[1] - VALS[0]
 
 print '*** Computing coefficients\n'
-Coef=zeros(shape(A)[1])
-RR=array(map(ps.get_N,SP))
-for i in xrange(Ngal): Coef[RR[i,1,:]]+=dVals*RR[i,0,:]
+Coef = zeros(shape(A)[1])
+RR = array(map(ps.get_N, SP))
+for i in xrange(Ngal): Coef[RR[i, 1, :]] += dVals * RR[i, 0, :]
 
-N_z=dot(A,Coef)
-N_z=N_z/sum(N_z)/dz
+N_z = dot(A, Coef)
+N_z = N_z / sum(N_z) / dz
 
 # Saving dN/dz to a file
 # the first column is the center of the dz bin and the second colum tne value
 # sum(N_z*dz) = 1
-if args.mask==None:
-    fileout='N_z_'+range_z+'_tpz.txt'
-    maskout='masked_data_'+range_z+'.npy'
+if args.mask == None:
+    if args.ids == None:
+        fileout = 'N_z_' + range_z + '_tpz.txt'
+        maskout = 'masked_data_' + range_z + '.npy'
+    else:
+        fileout = 'N_z_masked_ids_tpz.txt'
+        maskout = 'masked_data_ids.npy'
 else:
-    fileout='N_z_masked_tpz.txt'
-    maskout='masked_data.npy'
-    
-print '*** Output Files: \n'
-print '    N_z txt : '+fileout
-print '    Numpy mask : '+ maskout
-if args.return_pdfs:
-    print '    PDFs file : '+ pdfout
-    
+    fileout = 'N_z_masked_tpz.txt'
+    maskout = 'masked_data.npy'
 
-with open(fileout,'wb') as fout:
+print '*** Output Files: \n'
+print '    N_z txt : ' + fileout
+print '    Numpy mask : ' + maskout
+if args.return_pdfs:
+    print '    PDFs file : ' + pdfout
+
+with open(fileout, 'wb') as fout:
     fout.write(b'# zmid   n(z)\n')
-    ngal='# No galaxies : %d \n' %Ngal
+    ngal = '# No galaxies : %d \n' % Ngal
     fout.write(ngal)
-    savetxt(fout,zip(z,N_z),fmt='%.6f')
+    savetxt(fout, zip(z, N_z), fmt='%.6f')
 
 
 # Ploting dN/dz
 #
 if args.plot:
     plt.figure()
-    plt.fill_between(z,N_z,alpha=0.5,facecolor='red')
-    plt.xlabel(r'$z$',fontsize=20)
-    plt.ylabel(r'$dn(z)/dz$',fontsize=20)
-    plt.axvline(Mk['TPZ_ZPHOT'][0],linestyle='--',color='black')
-    plt.axvline(Mk['TPZ_ZPHOT'][1],linestyle='--',color='black')
+    plt.fill_between(z, N_z, alpha=0.5, facecolor='red')
+    plt.xlabel(r'$z$', fontsize=20)
+    plt.ylabel(r'$dn(z)/dz$', fontsize=20)
+    plt.axvline(Mk['TPZ_ZPHOT'][0], linestyle='--', color='black')
+    plt.axvline(Mk['TPZ_ZPHOT'][1], linestyle='--', color='black')
     plt.show()
--- mask.py
+++ mask.py
@@ -1,14 +1,13 @@
 # Use for masking objects from catalog
 #
-__author__="Matias Carrasco Kind"
+__author__ = "Matias Carrasco Kind"
 from numpy import *
 import pyfits as pf
 import matplotlib.pyplot as plt
-import sys,os
+import sys, os
 
 
-def get_mask(cuts, filename, output_keys='',input_ids=''):
-
+def get_mask(cuts, filename, output_keys='', input_ids=''):
     if cuts.has_key('MODEST_CLASS') and cuts.has_key('TPZ_SG_CLASS'):
         print '***********************'
         print '*        ERROR        *'
@@ -21,59 +20,59 @@ def get_mask(cuts, filename, output_keys='',input_ids=''):
 
         sys.exit(0)
 
-    G=pf.open(filename)
+    G = pf.open(filename)
     print '*** Info about the file...\n'
     print G.info()
     #print G[1].columns
-    if input_ids=='':
-        TB=G[1].data
-        mask=ones(G[1].header['NAXIS2'],dtype='bool')
+    if input_ids == '':
+        TB = G[1].data
+        mask = ones(G[1].header['NAXIS2'], dtype='bool')
         G.close()
         for k in cuts.keys():
-            if k.find('-') ==-1:
-                if len(cuts[k]) ==2:
-                    mtemp=TB.field(k) >= cuts[k][0]
-                    mask*=mtemp
-                    mtemp=TB.field(k) <= cuts[k][1]
-                    mask*=mtemp
+            if k.find('-') == -1:
+                if len(cuts[k]) == 2:
+                    mtemp = TB.field(k) >= cuts[k][0]
+                    mask *= mtemp
+                    mtemp = TB.field(k) <= cuts[k][1]
+                    mask *= mtemp
                 else:
-                    mtemp=TB.field(k) == cuts[k][0]
-                    mask*=mtemp
+                    mtemp = TB.field(k) == cuts[k][0]
+                    mask *= mtemp
             else:
-                minus=k.find('-')
-                mtemp=(TB.field(k[:minus]) - TB.field(k[minus+1:])) > cuts[k][0]
-                mask*=mtemp
-                minus=k.find('-')
-                mtemp=(TB.field(k[:minus]) - TB.field(k[minus+1:])) < cuts[k][1]
-                mask*=mtemp
-        output={}
-        output['MASK']=where(mask*1.==1)[0]
-        if output_keys!='':
+                minus = k.find('-')
+                mtemp = (TB.field(k[:minus]) - TB.field(k[minus + 1:])) > cuts[k][0]
+                mask *= mtemp
+                minus = k.find('-')
+                mtemp = (TB.field(k[:minus]) - TB.field(k[minus + 1:])) < cuts[k][1]
+                mask *= mtemp
+        output = {}
+        output['MASK'] = where(mask * 1. == 1)[0]
+        if output_keys != '':
             for k in output_keys:
                 if k.find('-') == -1:
-                    output[k]=TB.field(k)[mask]
+                    output[k] = TB.field(k)[mask]
                 else:
-                    minus=k.find('-')
-                    output[k]=TB.field(k[:minus])[mask]-TB.field(k[minus+1:])[mask]
+                    minus = k.find('-')
+                    output[k] = TB.field(k[:minus])[mask] - TB.field(k[minus + 1:])[mask]
         del mask, TB
         return output
     else:
-        TB=G[1].data
-        mask=zeros(G[1].header['NAXIS2'],dtype='bool')
+        TB = G[1].data
+        mask = zeros(G[1].header['NAXIS2'], dtype='bool')
         G.close()
-        in_ids=loadtxt(input_ids,dtype='int')
+        in_ids = loadtxt(input_ids, dtype='int')
         for k in in_ids:
-            mtemp=TB.field('COADD_OBJECTS_ID')==k
-            mask+=mtemp
-        output={}
-        output['MASK']=where(mask*1.==1)[0]
-        if output_keys!='':
+            mtemp = TB.field('COADD_OBJECTS_ID') == k
+            mask += mtemp
+        output = {}
+        output['MASK'] = where(mask * 1. == 1)[0]
+        if output_keys != '':
             for k in output_keys:
                 if k.find('-') == -1:
-                    output[k]=TB.field(k)[mask]
+                    output[k] = TB.field(k)[mask]
                 else:
-                    minus=k.find('-')
-                    output[k]=TB.field(k[:minus])[mask]-TB.field(k[minus+1:])[mask]
+                    minus = k.find('-')
+                    output[k] = TB.field(k[:minus])[mask] - TB.field(k[minus + 1:])[mask]
         del mask, TB
         return output
 
